959803c6b4830d2cf9eb7215374f647f03eb2e33
NedloG <daniil20020928@gmail.com>
2020-05-11 01:26:13 +0300

    Random array max value in a row

---
+++ src/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] args) {
+    System.out.println("Task 11 execution! A random size array is generated! \n'Main.Calculate' will find maximum in each row");
+    Main.Calculate();
+    }
+}
